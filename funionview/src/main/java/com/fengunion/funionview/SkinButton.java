1ed1241e83ef44ea131fa57bd97ea52004e7e77e
wangwu59105 <wangwu59105@qq.com>
2018-06-15 09:42:43 +0800

    Initial commit

---
+++ funionview/src/main/java/com/fengunion/funionview/SkinButton.java
@@ -0,0 +1,22 @@
+package com.fengunion.funionview;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+/**
+ * Created by wangwu on 2018/6/15.
+ */
+public class SkinButton extends AppCompatButton {
+    public SkinButton(Context context) {
+        super(context);
+    }
+
+    public SkinButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SkinButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ skin/src/main/java/com/fengunion/skin/SkinUtils.java
@@ -0,0 +1,8 @@
+package com.fengunion.skin;
+
+/**
+ * Created by wangwu on 2018/6/15.
+ */
+public class SkinUtils {
+
+}
